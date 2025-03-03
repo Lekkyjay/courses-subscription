90a46e6003879dfc5f5c066bef5194c091325838
‘Lekky <lekkyjay@yahoo.com>
2025-03-03 23:06:53 +0100

    Convex and clerk integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ConvexClientProvider } from "@/providers/ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ConvexClientProvider>
-          {children}
+          <ClerkProvider>
+            {children}
+          </ClerkProvider>
         </ConvexClientProvider>
       </body>
     </html>

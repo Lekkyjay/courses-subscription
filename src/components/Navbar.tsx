d27ee4315c9919efba3f5100d38d7918b2943fa8
‘Lekky <lekkyjay@yahoo.com>
2025-03-10 17:41:00 +0100

    Updated app title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <nav className='flex justify-between items-center py-4 px-6 bg-background border-b'>
 			<Link href={"/"} className='text-xl font-extrabold text-primary flex items-center gap-2'>
-				MasterClass <GraduationCap className='size-6' />
+				MasterCourses <GraduationCap className='size-6' />
 			</Link>
 
 			<div className='flex items-center space-x-1 sm:space-x-4'>
